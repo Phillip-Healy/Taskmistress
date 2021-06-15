efd93e4dab184a2b0afa16de6c9a839c92d228b2
Phillip-Healy <phillip.healy9@mail.dcu.ie>
2021-06-15 10:09:49 +0000

    customised add task form with input fields that match requirements and styles.

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,12 @@
     $(".sidenav").sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 5,
+        showClearBtn: true,
+        ii8n: {
+            done: "select"
+        }
+    });
   });
